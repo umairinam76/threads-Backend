77366bc6ec7af8419830e983066092b829251dfe
umairinam76 <umairinam76@gmail.com>
2024-05-07 13:58:13 +0500

    added new data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function init(){
 
@@ -13,11 +14,26 @@ const gqlServer = new ApolloServer({
     type Query{
         hello:String
         say(name:String):String
-    }`,
+    }
+    type Mutation{
+        createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
+    }
+    
+    `,
     resolvers:{
         Query:{
             hello:()=>'Hey im a graphql server...',
             say:(_,{name})=>`Hey ${name}`
+        },
+        Mutation:{
+            createUser:async(_,{firstName,lastName,email,password}:{firstName:string,lastName:string,email:string,password:string})=>{
+             await prismaClient.user.create({
+            data:{
+              firstName,lastName,email,password,salt:"random_salt"
+            },
+         });
+         return true;
+            }
         }
     }
 });
